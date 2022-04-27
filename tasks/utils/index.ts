d318da472a45f0110545312cb733ad245253008f
Mike <vinertsev@gmail.com>
2022-04-27 15:06:18 +0700

    fix: name task

--- tasks/utils/index.ts
+++ tasks/utils/index.ts
@@ -1,1 +1,2 @@
 import "./accounts"
+import "./mnemonic"
--- tasks/utils/mnemonic.ts
+++ tasks/utils/mnemonic.ts
@@ -1,9 +1,6 @@
 import { task } from "hardhat/config"
 
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-    const accounts = await hre.ethers.getSigners()
-
-    for (const account of accounts) {
-        console.log(account.address)
-    }
+task("mnemonic", "Generate new mnemonic", async (taskArgs, hre) => {
+    const wallet = hre.ethers.Wallet.createRandom()
+    console.log(`Mnemonic: ${wallet.mnemonic.phrase}`)
 })
